e75f4d02eaba470831b56ec7094a586dc7f507b2
MAYANK AGARWAL <66783755+mayankk1703@users.noreply.github.com>
2022-03-31 16:00:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomno=Math.floor(Math.random()*6)+1;
+var imgname="images/dice"+randomno+".png";
+document.querySelectorAll("img")[0].setAttribute("src",imgname);
+var random2=Math.floor(Math.random()*6)+1;
+var img2="images/dice"+random2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",img2);
+if(randomno==random2)
+{
+    document.querySelectorAll("h1")[0].innerText="Draw";
+}
+else if(randomno>random2)
+{
+    document.querySelectorAll("h1")[0].innerText="Player 1 wins";
+}
+else{
+    document.querySelectorAll("h1")[0].innerText="Player 2 wins";
+
+}
